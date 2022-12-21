5d49c1db51fbcc8e422e267203ee27ec5a53e7b4
Quim Nieto <quimnieto@MacBook-Pro-de-Tecnico.local>
2022-12-21 08:52:00 +0100

    Autowire config

--- apps/backoffice/backend/src/Controller/Users/UsersGetController.php
+++ apps/backoffice/backend/src/Controller/Users/UsersGetController.php
@@ -4,16 +4,23 @@ declare(strict_types=1);
 
 namespace SupremeUmbrella\Apps\Backoffice\Backend\Controller\Users;
 
+use SupremeUmbrella\Backoffice\Users\Application\Search\AllUsersSearcher;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 
 final class UsersGetController
 {
+
+    public function __construct(private Readonly AllUsersSearcher $searcher)
+    {
+    }
+
     public function __invoke(Request $request): JsonResponse
     {
         return new JsonResponse(
             [
                 'users_get' => 'ok',
+                'user' => $this->searcher->searchAll()
             ]
         );
     }
--- src/Backoffice/Users/Application/Search/AllUsersSearcher.php
+++ src/Backoffice/Users/Application/Search/AllUsersSearcher.php
@@ -4,16 +4,10 @@ declare(strict_types=1);
 
 namespace SupremeUmbrella\Backoffice\Users\Application\Search;
 
-use SupremeUmbrella\Backoffice\Users\Domain\UserRepository;
-
 final class AllUsersSearcher
 {
-    public function __construct(private Readonly UserRepository $repository)
+    public function searchAll(): string
     {
-    }
-
-    public function searchAll(): array
-    {
-        return $this->repository->searchAll();
+        return 'test';
     }
 }
